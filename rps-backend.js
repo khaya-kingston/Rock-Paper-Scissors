627770700339a0a9123457ffde01a01c6e2ad00a
Khaya Kingston <khaya.kingston@gmail.com>
2023-07-06 19:38:09 -0700

    current functionality using DOMs up and running

--- rps-backend.js
+++ rps-backend.js
@@ -1,65 +1,83 @@
 
-
-let computerMove;
-
-let playerMove = "r0ck";
-
-let score = 0;
+// Access the round, score, and comp score on the scoreboard
+let score = document.querySelector('.score-box .score');
+let compScore = document.querySelector('.score-box .computer');
+let screenPut = document.querySelector('#screen');
 
 
+// Generate local variables to keep track and update the screen
+let localPlayerScore = 0;
+let computerScore = 0;
+
+
+// Possible Moves
 const moves = ["Rock", "Paper", "Scissors"];
 
-function moveCleaner(move) {
-    if (typeof move != "string") {
-        move = "";
-    } else {
-        move = move.charAt(0).toUpperCase() + move.slice(1).toLowerCase();
-    }
-    return move;
-}
-
-
-
+// Generate a computer move
 function getComputerChoice() {
     return moves[Math.floor(Math.random()*moves.length)];
 }
 
 
-function playRound(computerMove, playerMove) {
+// Update the scoreboard given a current play
+function update(outCome) {
     
-    playerMove = moveCleaner(playerMove);
+    switch (outCome){
 
-    if (!moves.includes(playerMove)) {
-        return "You Lose! You didn't play a valid move";
+        case "Lose":
+            computerScore++;
+            compScore.textContent = computerScore.toString();
+            break;
+
+        case "Win":
+            localPlayerScore++;
+            score.textContent = localPlayerScore.toString();
+            break;
+
+        default:
+            break;
     }
+}
+
+
+function endGame() {
+    if (localPlayerScore === computerScore) {
+        screenPut.textContent = "You Tied";
+    } else if (localPlayerScore < computerScore) {
+        screenPut.textContent = "You Lost to a Computer";
+    } else {
+        screenPut.textContent = "You WIN!!!!";
+    }
+}
+
+function playRound(playerMove) {
     
+    let computerMove = getComputerChoice();
+
     if (playerMove === computerMove) {
-        return "Tie!"
+        update("Tie");
     }
 
     if ((playerMove === "Rock" && computerMove === "Scissors") ||
     (playerMove === "Paper" && computerMove === "Rock") ||
     (playerMove == "Scissors" && computerMove === "Paper")) {
-        score++;
-        return "You Win! ".concat(playerMove, " beats ", computerMove);
+        update("Win");
     }
     else {
-        return "You Lose! ".concat(computerMove, " beats ", playerMove);
+        update("Lose");
+    }
+
+    if (localPlayerScore === 5) {
+        endGame();
     }
 }
 
-function game() {
-    for (i = 0; i < 5; i++) {
-        playerMove = prompt("What is your move?", "Ex: Rock");
-        computerMove = getComputerChoice();
-        console.log(playRound(computerMove, playerMove));
-        console.log(score);
-    }
-    if (score > 2) {
-        console.log("You Beat the Computer!");
-    } else {
-        console.log("You Lost You Moron.");
-    }
-}
 
-game();
+// Access all buttons on the screen and assign the play for a click
+let buttons = document.querySelectorAll('.big-button');
+// assign a listener to play a round on each click
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        playRound(button.id);
+    });
+});
